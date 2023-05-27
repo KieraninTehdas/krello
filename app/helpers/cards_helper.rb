1d836c61f3d7987a2c73b71befea8b30e82255ec
Kieran Porter <kieranintehdas@gmail.com>
2023-05-27 23:57:49 +0100

    Shift turbo frame dom ids into helpers

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module CardsHelper
+  def new_card_id(swimlane)
+    "swimlane_#{swimlane.id}_new_card"
+  end
+
+  def last_card_id(swimlane)
+    "swimlane_#{swimlane.id}_last_card"
+  end
 end
--- app/helpers/swimlanes_helper.rb
+++ app/helpers/swimlanes_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module SwimlanesHelper
+  def last_swimlane_id
+    "last_swimlane"
+  end
+
+  def new_swimlane_id
+    "new_swimlane"
+  end
 end
