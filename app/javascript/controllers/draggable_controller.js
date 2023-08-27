e10c8044229f501649d271bebc398dc1b5672320
Kieran Porter <kieranintehdas@gmail.com>
2023-08-27 23:03:15 +0100

    Semi-working drag and drop: works within lane, but not to different lanes. No calls to update on server wired in

--- app/javascript/controllers/draggable_controller.js
+++ app/javascript/controllers/draggable_controller.js
@@ -25,12 +25,45 @@ export default class extends Controller {
 
   drop(event) {
     event.preventDefault();
-
     console.log(`Dropping in ${event.target}`);
-    if (event.target.className === "card-container") {
-      this.cardTarget.parentNode.removeChild(this.cardTarget);
-      event.target.appendChild(this.cardTarget);
+
+    const dropTarget = event.target;
+    const draggedItemId = event.dataTransfer.getData("application/drag-key");
+    console.log(`Dragged item id: ${draggedItemId}`);
+
+    let draggedItem = this.element.querySelector(
+      `[data-card-id='${draggedItemId}']`
+    );
+
+    if (!!draggedItem) {
+      this.moveWithinLane(dropTarget, draggedItem);
+    } else {
+      this.moveToNewLane(dropTarget, draggedItemId);
     }
+  }
+
+  moveWithinLane(dropTarget, draggedItem) {
+    const positionComparison = dropTarget.compareDocumentPosition(draggedItem);
+    if (positionComparison & 4) {
+      dropTarget.insertAdjacentElement("beforebegin", draggedItem);
+    } else if (positionComparison & 2) {
+      dropTarget.insertAdjacentElement("afterend", draggedItem);
+    }
+  }
+
+  moveToNewLane(dropTarget, draggedItemId) {
+    const draggedItem = document.querySelector(
+      `[data-card-id='${draggedItemId}']`
+    );
+    const positionComparison = dropTarget.compareDocumentPosition(draggedItem);
+
+    console.log(positionComparison);
+    if (positionComparison & 4) {
+      dropTarget.insertAdjacentElement("beforebegin", draggedItem);
+    } else if (positionComparison & 2) {
+      dropTarget.insertAdjacentElement("afterend", draggedItem);
+    }
+    dropTarget.insertAdjacentElement("afterbegin", draggedItem);
   }
 
   dragEnd(event) {}
